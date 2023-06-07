6ed6f685443ef8cc0732080e2a2a2f4bae1c3c8d
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2023-06-07 15:00:28 +0100

    fix(poc): remove debug printing

--- src/poc/heapless/field_def.c
+++ src/poc/heapless/field_def.c
@@ -3,13 +3,10 @@
 #include "field_def.h"
 #include "../field.h"
 
-#include <stdio.h>
-
 field new_field(unsigned int size) {
     field f;
     f.size = size;
     f.num_tiles = size * size;
-    printf("num_tiles: %d\n", f.num_tiles);
 
     for (int i = 0; i < f.num_tiles; i++) {
         f.inner[i] = f.num_tiles;
